0b9574dbd3ddf2497c3769077e2ae6fb0b67e8b5
Ankit Yadav <ankityadav1252002@gmail.com>
2024-09-22 20:02:10 +0530

    first commit

---
+++ src/components/Data.jsx
@@ -0,0 +1,333 @@
+export const items = [
+  {
+    id: 17,
+    category: "laptops",
+    title: "HP Laptop 15, 13th Gen",
+    imgSrc:
+      "https://m.media-amazon.com/images/I/41RekcJPniL._SX300_SY300_QL70_FMwebp_.jpg",
+    description:
+      " Core™ i5-1334U, 15.6-inch(39.6 cm), FHD, 8GB DDR4(3200 MHZ),512GB SSD",
+    price: "51,999",
+  },
+  {
+    id: 1,
+    category: "mobiles",
+    title: "Apple iPhone 14",
+    imgSrc: "https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg",
+    description: "Apple iPhone 14 (128 GB) - Blue",
+    price: "89999",
+  },
+  {
+    id: 22,
+    category: "headphones",
+    title: "boAt Rockerz 425",
+    imgSrc: "https://m.media-amazon.com/images/I/61XvYOrqVeL._SX522_.jpg",
+    description: " Bluetooth Wireless Over Ear Headphones with Mic",
+    price: "1,499",
+  },
+  {
+    id: 2,
+    category: "mobiles",
+    // category: 'headphones',
+    title: "Apple iPhone 11",
+    imgSrc: "https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg",
+    description: "Apple iPhone 11 (64GB) - White",
+    price: "79999",
+  },
+
+  {
+    id: 32,
+    category: "earbuds",
+    title: " Boult",
+    imgSrc:
+      "https://m.media-amazon.com/images/I/61b8STGCCxL._AC_UL480_FMwebp_QL65_.jpg",
+    description: "Boult Audio Z60 Earbuds with 60H Battery",
+    price: "1,900",
+  },
+  {
+    id: 3,
+    category: "mobiles",
+    title: "Apple iPhone 13",
+    imgSrc: "https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg",
+    description: "Apple iPhone 13 (128GB) - Blue",
+    price: "69999",
+  },
+
+  {
+    id: 12,
+    category: "mobiles",
+    title: "Apple iPhone SE",
+    imgSrc:
+      "https://m.media-amazon.com/images/I/31eq5m7D5uL._SY445_SX342_QL70_FMwebp_.jpg",
+    description: "Apple iPhone SE (64 GB) - Midnight (3rd Generation)",
+    price: "24,999",
+  },
+
+  {
+    id: 13,
+    category: "mobiles",
+    title: "Redmi 12 5G",
+    imgSrc: "https://m.media-amazon.com/images/I/81SocKy8ghL._SL1500_.jpg",
+    description: "Redmi 12 5G Moonstone Silver 8GB RAM 256GB ROM",
+    price: "13,999",
+  },
+
+  {
+    id: 14,
+    category: "mobiles",
+    title: "realme NARZO N65 5G",
+    imgSrc: "https://m.media-amazon.com/images/I/71ocQY+G2XL._SX679_.jpg",
+    description:
+      "Amber Gold 8GB RAM, 128GB Storage India 1st D6300 5G Chipset ",
+    price: "13,499",
+  },
+
+  {
+    id: 15,
+    category: "mobiles",
+    title: "realme NARZO N61",
+    imgSrc: "https://m.media-amazon.com/images/I/71-NQgckRZL._SX679_.jpg",
+    description:
+      " (Marble Black,6GB RAM+128GB Storage) 90Hz Eye Comfort Display",
+    price: "8,499",
+  },
+
+  {
+    id: 16,
+    category: "mobiles",
+    title: "Samsung Galaxy Z Fold6 5G",
+    imgSrc: "https://m.media-amazon.com/images/I/61Hhrr0bFWL._SX679_.jpg",
+    description: "Silver Shadow, 12GB RAM, 1TB Storage",
+    price: "2,00,999",
+  },
+
+  {
+    id: 4,
+    category: "laptops",
+    title: "Lenovo IdeaPad Slim 5 ",
+    imgSrc: "https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg",
+    description: 'Lenovo IdeaPad Slim 5 Intel Core i5 12450H 14" (35.5cm) ',
+    price: "29999",
+  },
+
+  {
+    id: 11,
+    category: "mobiles",
+    title: "Redmi Note 13 5G",
+    imgSrc:
+      "https://m.media-amazon.com/images/I/71VW8LmqqPL._AC_UY327_FMwebp_QL65_.jpg",
+    description:
+      "Arctic White, 6GB RAM, 128GB Storage 120Hz Bezel-Less AMOLED ",
+    price: "16,999",
+  },
+
+  {
+    id: 18,
+    category: "laptops",
+    title: "Dell 15 Thin & Light Laptop",
+    imgSrc: "https://m.media-amazon.com/images/I/81T5jRKDkvL._SX679_.jpg",
+    description:
+      ', 12th Gen Intel Core i5-1235U Processor, 8GB, 512GB SSD, 15.6" (38) cm FHD Display, Windows 11 + MSO',
+    price: "46,999",
+  },
+
+  {
+    id: 18,
+    category: "laptops",
+    title: "ASUS TUF Gaming A15",
+    imgSrc: "https://m.media-amazon.com/images/I/71RL6QQz2BL._SX679_.jpg",
+    description:
+      ', 15.6" (39.62cm) FHD 16:9 144Hz, AMD Ryzen 7 7435HS, Gaming Laptop (16 DDR5/512GB SSD',
+    price: "66,999",
+  },
+  {
+    id: 5,
+    category: "laptops",
+    title: "Xiaomi [Smartchoice] Notebookpro",
+    imgSrc: "https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg",
+    description: "Xiaomi [Smartchoice] Notebookpro ",
+    price: "49999",
+  },
+
+  {
+    id: 6,
+    category: "tablets",
+    title: "Lenovo Tab P12 Pro AMOLED",
+    imgSrc: "https://m.media-amazon.com/images/I/51b9LjzmPCL._SL1080_.jpg",
+    description:
+      "Lenovo Tab P12 Pro AMOLED (12.6 inch (32 cm), 8 GB, 256 GB, Wi-fi Only), Storm Grey with Precision Pen 3",
+    price: "19,999",
+  },
+
+  {
+    id: 19,
+    category: "tablets",
+    title: "Samsung Galaxy Tab ",
+    imgSrc: "https://m.media-amazon.com/images/I/51kwgzu4NsL._SX679_.jpg",
+    description:
+      "A9+ 27.94 cm (11.0 inch) Display, RAM 8 GB, ROM 128 GB Expandable, Wi-Fi Tablet, Graphite Upto INR 4500 Bank Discount",
+    price: "18,999",
+  },
+
+  {
+    id: 20,
+    category: "tablets",
+    title: "OnePlus Pad",
+    imgSrc: "https://m.media-amazon.com/images/I/21d6aNv3T2L.jpg",
+    description:
+      "12Gb Ram,256Gb Storage, Mediatek Dimensity 9000, Android 13.1, 144Hz Refresh Rate, Dolby Vision Atmos",
+    price: "29,999",
+  },
+
+  {
+    id: 21,
+    category: "tablets",
+    title: "Redmi Pad Pro",
+    imgSrc: "https://m.media-amazon.com/images/I/51mj0WGMBdL._SX679_.jpg",
+    description: "6GB, 128GB| Quad Speakers|Wi-Fi 6| Graphite Grey",
+    price: "21,999",
+  },
+
+  {
+    id: 7,
+    category: "tablets",
+    title: "Xiaomi Pad 6",
+    imgSrc: "https://m.media-amazon.com/images/I/51b9LjzmPCL._SL1080_.jpg",
+    description: "Xiaomi Pad 6| Qualcomm Snapdragon 870| ",
+    price: "29999",
+  },
+
+  {
+    id: 8,
+    category: "laptops",
+    title: "Apple MacBook Air Laptop",
+    imgSrc: "https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg",
+    description:
+      "Apple MacBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display, ",
+    price: "89999",
+  },
+
+  {
+    id: 9,
+    category: "tablets",
+    title: "Samsung Galaxy Tab A8",
+    imgSrc: "https://m.media-amazon.com/images/I/51b9LjzmPCL._SL1080_.jpg",
+    description:
+      "Samsung Galaxy Tab A8 26.69cm (10.5 inch) Display, RAM 4 GB, ROM 64 GB Expandable, Wi-Fi Tablet, Gray, (SM-X200NZAEINU)",
+    price: "49999",
+  },
+
+  {
+    id: 10,
+    category: "headphones",
+    title: "Noise 13",
+    imgSrc:
+      "https://www.gonoise.com/cdn/shop/files/Noise-three---carousel-2_c1b62e6d-6498-4b2f-bf55-155361f190a8.png?v=1701345285",
+    description: "Apple iPhone 14 (128 GB) - Blue",
+    price: "999",
+  },
+
+  {
+    id: 23,
+    category: "headphones",
+    title: "ZEBRONICS Zeb-Bang PRO",
+    imgSrc:
+      "https://m.media-amazon.com/images/I/41sYZoLBovL._SX300_SY300_QL70_FMwebp_.jpg",
+    description:
+      "  Bluetooth v5.0 On Ear Headphone, 30H Backup, Foldable Design, Call Function, Voice Assistant Feature",
+    price: "799",
+  },
+
+  {
+    id: 24,
+    category: "headphones",
+    title: " Sony WH-CH720N",
+    imgSrc: "https://m.media-amazon.com/images/I/31+CMjgVyHL._SY300_SX300_.jpg",
+    description:
+      " Wireless Over-Ear Active Noise Cancellation Headphones with Mic, up to 35 Hours Playtime",
+    price: "8,499",
+  },
+
+  {
+    id: 25,
+    category: "televison",
+    title: " Panasonic",
+    imgSrc:
+      "https://m.media-amazon.com/images/I/41BqvH8jFUL._SX300_SY300_QL70_FMwebp_.jpg",
+    description:
+      " Panasonic 108 cm (43 inches) 4K Ultra HD Smart LED Google TV TH-43MX660DX (Black)",
+    price: "29,900",
+  },
+
+  {
+    id: 26,
+    category: "televison",
+    title: " Samsung",
+    imgSrc:
+      "https://m.media-amazon.com/images/I/51c8uSMKOhL._SY300_SX300_QL70_FMwebp_.jpg",
+    description:
+      " Samsung 108 cm (43 inches) D Series Crystal 4K Vivid Ultra HD Smart LED TV UA43DUE70BKLXL (Black)",
+    price: "29,999",
+  },
+
+  {
+    id: 27,
+    category: "televison",
+    title: " LG",
+    imgSrc: "https://m.media-amazon.com/images/I/41G5s4uf+AL._SX300_SY300_.jpg",
+    description:
+      "LG 80 cm (32 inches) HD Ready Smart LED TV 32LM563BPTC (Dark Iron Gray)",
+    price: "14,900",
+  },
+  {
+    id: 15,
+    category: "mobiles",
+    title: "realme NARZO N61",
+    imgSrc: "https://m.media-amazon.com/images/I/71-NQgckRZL._SX679_.jpg",
+    description:
+      " (Marble Black,6GB RAM+128GB Storage) 90Hz Eye Comfort Display",
+    price: "8,499",
+  },
+  {
+    id: 28,
+    category: "earbuds",
+    title: " boAt Airdopes 311 Pro",
+    imgSrc:
+      "https://m.media-amazon.com/images/I/31TJgLgwsXL._SX300_SY300_QL70_FMwebp_.jpg",
+    description:
+      "boAt Airdopes 311 Pro Truly Wireless in Ear Ear Buds w/Up to 50 HRS Playtime",
+    price: "1,200",
+  },
+
+  {
+    id: 29,
+    category: "earbuds",
+    title: " OnePlus Nord Buds",
+    imgSrc:
+      "https://m.media-amazon.com/images/I/31hXqtvutdL._SX300_SY300_QL70_FMwebp_.jpg",
+    description:
+      "OnePlus Nord Buds 2r True Wireless in Ear Earbuds with Mic, 12.4mm Drivers",
+    price: "1,980",
+  },
+
+  {
+    id: 30,
+    category: "earbuds",
+    title: " Fastrack Fpods FZ100",
+    imgSrc:
+      "https://m.media-amazon.com/images/I/31kAPxXEpgL._SX300_SY300_QL70_FMwebp_.jpg",
+    description:
+      "Fastrack Fpods FZ100 TWS in-Ear Earbuds with Mega 50 Hrs Playtime|Extra Deep Bass Driver",
+    price: "2,900",
+  },
+
+  {
+    id: 31,
+    category: "earbuds",
+    title: " Redmi Buds 5",
+    imgSrc: "https://m.media-amazon.com/images/I/61la7rfN9eL._SX679_.jpg",
+    description:
+      "Redmi Buds 5 Bluetooth TWS Earbuds, Up to 46dB Hybrid Noise Cancellation",
+    price: "2,500",
+  },
+];
